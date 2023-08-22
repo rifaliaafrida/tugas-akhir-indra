a2886d73dc7a92362230e22cbe66d139239cfddd
Rifa12art <rayajina029@gmail.com>
2023-08-22 14:51:42 +0700

    navbar

--- script/script.js
+++ script/script.js
@@ -5,3 +5,33 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     mobileMenu.classList.toggle('active');
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const allProductBtn = document.getElementById("allProductBtn");
+    const manBtn = document.getElementById("spritBtn");
+    const womenBtn = document.getElementById("lornaBtn");
+    const productItems = document.querySelectorAll(".product");
+
+    function showProducts(category) {
+        productItems.forEach(item => {
+            if (item.classList.contains(category) || category === "all") {
+                item.style.display = "block";
+            } else {
+                item.style.display = "none";
+            }
+        });
+    }
+
+    allProductBtn.addEventListener("click", function() {
+        showProducts("all");
+    });
+
+    manBtn.addEventListener("click", function() {
+        showProducts("sprit");
+    });
+
+    womenBtn.addEventListener("click", function() {
+        showProducts("lorna");
+    });
+});
